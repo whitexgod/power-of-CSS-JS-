b4fb3c6ba0d3615e5f2116d5e28d4f5caea12084
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-07-27 17:22:33 +0530

    Add files via upload

--- custom nav/main.js
+++ custom nav/main.js
@@ -7,12 +7,12 @@ const navSlide = function() {
   burger.addEventListener('click', function() {
     nav.classList.toggle('nav-active');
 
-    navLinks.forEach((link, index), function(){
+    navLinks.forEach((link, index) => {
       if (link.style.animation){
         link.style.animation='';
       }
       else{
-        link.style.animation = `navLinkFade 0.5s ease forward ${index / 7 + 1}s`;
+        link.style.animation = `change 0.3s ease forwards ${index / 7 + 1}s`;
       }
     });
     burger.classList.toggle('toggle');
