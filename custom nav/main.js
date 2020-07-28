0e73b31131a732c038d33fbbe03075851c1cc300
Tuhin Mukherjee <59216687+whitexgod@users.noreply.github.com>
2020-07-28 08:30:46 +0530

    Add files via upload

--- custom nav/main.js
+++ custom nav/main.js
@@ -19,4 +19,30 @@ const navSlide = function() {
   });
 };
 
-navSlide();
+const navClose = function(){
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  nav.addEventListener('click', function(){
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation){
+        link.style.animation='';
+      }
+      else{
+        link.style.animation = `change 0.3s ease forwards ${index / 7 + 1}s`;
+      }
+    });
+
+    burger.classList.toggle('toggle');
+  });
+};
+
+const run = function(){
+  navSlide();
+  navClose();
+};
+
+run();
